2582ccb6d0f4448f07cbe7b154cfd5cd47a549d6
Alex Maksimov <maleksware@gmail.com>
2023-01-11 13:22:03 +1100

    Add English variants and Headquarters

--- background.js
+++ background.js
@@ -13,6 +13,20 @@ const replacement = {
   "Мастер": "♂️Full master♂️",
   "Гроссмейстер": "♂️Boss of the gym♂️",
   "Международный гроссмейстер": "♂️International boss of the gym♂️",
-  "Легендарный гроссмейстер": "♂️Legendary boss of the gym♂️"
+  "Легендарный гроссмейстер": "♂️Legendary boss of the gym♂️",
+  "Штаб": "♂️Van Darkholme♂️",
+
+  "Unrated": "not a true ♂️man♂️",
+  "Newbie": "♂️fucking slave♂️",
+  "Pupil": "♂️boy next door♂️",
+  "Specialist": "♂️College boy♂️",
+  "Expert": "♂️Leatherman♂️",
+  "Candidate master": "♂️Dungeon master♂️",
+  "International master": "♂️Fantasy master♂️",
+  "Master": "♂️Full master♂️",
+  "Grandmaster": "♂️Boss of the gym♂️",
+  "International Grandmaster": "♂️International boss of the gym♂️",
+  "Legendary Grandmaster": "♂️Legendary boss of the gym♂️",
+  "Headquarters": "♂️Van Darkholme♂️"
 };
 text.innerText = replacement[text.innerText];
